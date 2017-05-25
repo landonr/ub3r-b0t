3fbc366efb58a48564b33a893b79a3dc5fd420c0
IQMETRIXHO\Landon.Rohatensky <landonroha@gmail.com>
2017-05-25 17:35:01 -0600

    fuk

---
+++ src/Bot.Slack.cs
@@ -0,0 +1,30 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UB3RB0T
+{
+    public partial class Bot
+    {
+		public void CreateSlackBot()
+		{
+			var bot = new Slackbot.Bot("xoxb-186062788998-4YRrImSSyDRvvMLUmYQt1DQ2", "robolando");
+
+			bot.OnMessage += (sender, message) =>
+			{
+				SocketMessage socketMessage;
+			}
+
+			bot.OnMessage += (sender, message) =>
+			{
+				if (message.MentionedUsers.Any(user => user == "robolando"))
+				{
+					bot.SendMessage(message.Channel, $"Hi {message.User}, thanks for mentioning my name!");
+				}
+			};
+		}
+	}
+}
